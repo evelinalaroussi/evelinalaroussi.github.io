1c8dc7c615ef0cd1fcffdab080ab8a0d652c5ddc
evelinalaroussi <laroussi@kth.se>
2017-08-10 19:26:19 +0200

    hejk

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Interactive from 'react-interactive';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
-import PageNotFound from './PageNotFound';
 import Header from './Header';
 import About from './About';
 //import s from '../styles/app.style';
@@ -15,7 +14,6 @@ export default function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/head" component={Header} />
         <Route exact path="/about" component={About} />
-        <Route component={PageNotFound} />
       </Switch>
 
     </div>
