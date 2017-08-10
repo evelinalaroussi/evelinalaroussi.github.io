ffbc751190d1077dd41af1faf28d2f34b40fbb5a
evelinalaroussi <laroussi@kth.se>
2017-08-10 14:30:00 +0200

    about added

---
+++ src/components/About.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Interactive from 'react-interactive';
+import { Link } from 'react-router-dom';
+//import { Code } from '../styles/style';
+//import s from '../styles/home.style';
+
+function About () {
+
+    return (
+<div id="container" style={{height: ""}}>
+<div><Link to="/header"><img className="logo-img" src="./logo_white.png"/></Link></div>
+<div className="head">ABOUT ME</div>
+<hr id="headLine"/>
+
+    <div className="meContainer">
+          <div id="meText">
+            <p>What i like the most about working with media technology is the impact it has in people. I think it is interesting to work with developing new concepts and to reach many people at the same time. The media technology industry is growing everyday and is an important part of our future. <span id="bigger">It is simply a development that i want to be a part of.</span> </p>
+            <p>As a front end developer, i truly enjoy the vision of an application or website coming together piece by piece. I love being challenged and learn more coding to get a broader skillset. Even though i love coding, i also have an intresst for interaction design.</p>
+      
+            <p>I am currently working as a front-end developer and UX designer at <a href="www.twebcast.com">Twebcast</a></p>
+          </div>
+      </div>
+
+
+  <div className = "skillcontainer">
+    <dl className="skills-diagram">
+      <dt className="skill-5">ReactJS</dt>
+      <dd>5</dd>
+      <dt className="skill-10">HTML & CSS</dt>
+      <dd>10</dd>
+      <dt className="skill-3">SQL</dt>
+      <dd>3</dd>
+      <dt className="skill-8">InteractionDesign</dt>
+      <dd>8</dd>
+      <dt className="skill-4">Photoshop</dt>
+      <dd>4</dd>
+      <dt className="skill-11">Python</dt>
+      <dd>4</dd>
+      <dt className="skill-6">JQuery</dt>
+      <dd>6</dd>
+      <dt className="skill-7">AngularJS</dt>
+      <dd>7</dd>
+      <dt className="skill-9">Javascript</dt>
+      <dd>9</dd>
+      <dt className="skill-2">Java</dt>
+      <dd>2</dd>
+    </dl>
+  </div>
+
+</div>
+    );
+  };
+
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import PageNotFound from './PageNotFound';
 import Header from './Header';
+import About from './About';
 
 //import s from '../styles/app.style';
 
@@ -14,6 +15,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/head" component={Header} />
+        <Route exact path="/about" component={About} />
         <Route component={PageNotFound} />
       </Switch>
 
