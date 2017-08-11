ce4815c5f2e5584094c1598a9af866884dd5a715
evelinalaroussi <laroussi@kth.se>
2017-08-11 13:48:26 +0200

    adding images

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,7 +9,19 @@ function Work () {
   
 
     return (
-      <div>work</div>
+      <div id="container">
+        <div id="work-container">
+          <div><img className="logo-img" src="./logo.png"/></div>
+          <div className="head">MY WORK AND PROJECTS</div>
+          <hr id="headLine"/>
+          <div className="images">
+            <img className="workImg" id="beer" src="./work_images/imgglass1.jpg"/>
+            <img className="workImg" id="unichat" src="./work_images/unichat.png"/>
+            <img className="workImg" id="meetingplanner" src="./work_images/meetingplanner.png"/>
+            <img className="workImg" src="./logo.png"/>
+          </div>
+        </div>
+    </div>
 )
   };
 
