29e0a693a91d261cd76a9e890497dc7b39062021
evelinalaroussi <laroussi@kth.se>
2017-08-12 15:17:25 +0200

    thissite

--- src/components/Thissite.js
+++ src/components/Thissite.js
@@ -14,8 +14,27 @@ export default class Work extends React.Component {
 
 render() {
     return (
-      <div id="container">
-      
+      <div id="siteContainer" className="thissite">
+      <div className = "this-img">
+        <div className="head"><p>THIS SITE</p></div>
+        <hr id="headLine"/>
+        <div className="button-div"><button id="button">Back to listing</button></div>
+        <img id="mcbook" src="./work_images/mcbook2.png"/>
+      </div>
+      <div className="this-text">
+          <div className="tech">
+            <h3>WHAT I USED</h3>
+            <hr id="miniheadLine"/>
+            <p>ReactJS + react routing</p>
+            <p>CSS</p>
+
+          </div>
+          <div className="desc"><h3>DESCRIPTION</h3>
+          <hr id="miniheadLine"/>
+          <p>I built this portfolio over the summer as a project to learn react, but also to improve my design skills </p>
+
+          </div>
+      </div>
     </div>
 )
 }
